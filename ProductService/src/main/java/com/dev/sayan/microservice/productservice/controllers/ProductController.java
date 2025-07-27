f44f249ed30d0651dfe1b89e69f2e6f546e41bf6
sayan4545 <sayanchatterjee3010@gmail.com>
2025-07-28 00:19:51 +0530

    Added RestTemplate object to replace the manual connection creation using HttpURLConnection object

--- ProductService/src/main/java/com/dev/sayan/microservice/productservice/controllers/ProductController.java
+++ ProductService/src/main/java/com/dev/sayan/microservice/productservice/controllers/ProductController.java
@@ -1,11 +1,13 @@
 package com.dev.sayan.microservice.productservice.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -16,35 +18,41 @@ import java.net.URL;
 
 public class ProductController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/{id}")
     public ResponseEntity<String> getOrderFromProduct(@PathVariable Long id){
 
-        HttpURLConnection  httpURLConnection = null;
+        String response =  restTemplate.getForObject("http://localhost:8081/"+id,String.class);
+        System.out.println("Response: " + response);
 
-        try{
-            String url = "http://localhost:8081/" + id;
-            URL obj = new URL(url);
-            httpURLConnection = (HttpURLConnection)obj.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.setRequestProperty("Accept", "application/json");
-            //httpURLConnection.setConnectTimeout(5);
-           // httpURLConnection.setReadTimeout(1000);
-            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String responseLine;
-            while((responseLine=in.readLine())!=null){
-                response.append(responseLine);
-            }
-            in.close();
-            System.out.println("Response: "+response.toString());
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        finally {
-            if(httpURLConnection!=null){
-                httpURLConnection.disconnect();
-            }
-        }
+//        HttpURLConnection  httpURLConnection = null;
+//
+//        try{
+//            String url = "http://localhost:8081/" + id;
+//            URL obj = new URL(url);
+//            httpURLConnection = (HttpURLConnection)obj.openConnection();
+//            httpURLConnection.setRequestMethod("GET");
+//            httpURLConnection.setRequestProperty("Accept", "application/json");
+//            //httpURLConnection.setConnectTimeout(5);
+//           // httpURLConnection.setReadTimeout(1000);
+//            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+//            StringBuilder response = new StringBuilder();
+//            String responseLine;
+//            while((responseLine=in.readLine())!=null){
+//                response.append(responseLine);
+//            }
+//            in.close();
+//            System.out.println("Response: "+response.toString());
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
+//        finally {
+//            if(httpURLConnection!=null){
+//                httpURLConnection.disconnect();
+//            }
+//        }
 
         return ResponseEntity.ok("Order call successful with id : " + id);
 
